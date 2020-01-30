9b1dd1f36c6501b8331ee015fb2aba0db9ed1734
Anna <honiara1951@gmail.com>
2020-01-30 13:36:11 +0200

    error eslint corrected

--- src/service/service.js
+++ src/service/service.js
@@ -1,4 +1,3 @@
-/* eslint-disable indent */
 'use strict';
 
 const {USER_ARGV_INDEX, DEFAULT_COMMAND, ExitCode} = require(`../constants`);
@@ -8,8 +7,8 @@ const userArguments = process.argv.slice(USER_ARGV_INDEX);
 const [userCommand] = userArguments;
 
 if (userArguments === 0 || !Cli[userCommand]) {
-    Cli[DEFAULT_COMMAND].run();
-    process.exit(ExitCode.success);
+  Cli[DEFAULT_COMMAND].run();
+  process.exit(ExitCode.success);
 }
 
 Cli[userCommand].run(userArguments.slice(1));
