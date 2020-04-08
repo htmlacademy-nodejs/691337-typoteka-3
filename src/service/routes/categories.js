9cce3010e82e3a054d22a8f56328151d4d426d06
Anna <honiara1951@gmail.com>
2020-04-08 22:18:10 +0300

    npm audit fixed, eslint error corrected

--- src/service/routes/categories.js
+++ src/service/routes/categories.js
@@ -3,7 +3,6 @@ const express = require(`express`);
 const controller = require(`../controllers/categories`);
 
 const categoriesRouter = new express.Router();
-//postsRouter.use(express.json());
 
 categoriesRouter.get(`/`, controller.getCategories);
 
